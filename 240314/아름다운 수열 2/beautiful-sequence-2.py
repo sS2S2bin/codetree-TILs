483531e2af8e27b36e8a95dc8580d9636538ad43
Soobin DD, (Designer + Developer) <52414079+sS2S2bin@users.noreply.github.com>
2024-03-14 16:15:57 +0900

    Title: 아름다운 수열 2, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,29 @@
+n,m = map(int,input().split())
+a_arr = list(map(int, input().split()))
+b_arr = list(map(int, input().split()))
+b_set = set(b_arr)
+b_cnt_dict = {}
+
+for i,b in enumerate(b_arr):
+    b_cnt_dict[b] = b_arr.count(b)
+
+continue_cnt_value = 0
+answer = 0 
+prev_bool = False
+for i in range(n-m+1):
+    for b in b_set:
+        # print(a_arr[i:i+m].count(b))
+        if a_arr[i:i+m].count(b)==b_cnt_dict[b]:
+            # print(a_arr[i:i+m],'{},a cnt : {}, b cnt : {}'.format(b,a_arr[i:i+m].count(b),b_cnt_dict[b]))
+            # print()
+            continue_cnt_value += a_arr[i:i+m].count(b)
+            prev_bool = True
+        else: 
+            continue_cnt_value = 0
+            prev_bool = False
+        if continue_cnt_value == m and prev_bool:
+            answer += 1
+            # print('~',a_arr[i:i+m],answer)
+            continue_cnt_value = 0
+
+print(answer)

279306bd020f6fc3a0d6309b4561420d22e0c094
Soobin DD, (Designer + Developer) <52414079+sS2S2bin@users.noreply.github.com>
2024-04-05 15:59:08 +0900

    Title: 문자에 따른 명령 2, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240405/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,16 @@
+import sys
+direction = list(sys.stdin.readline().strip())
+answer = [0,0]
+dr = [1,0,-1,0]
+dc = [0,1,0,-1]
+idx = 3
+for dirc in direction:
+    if dirc == 'L':
+        idx = (idx+3)%4
+    elif dirc == 'R':
+        idx = (idx+1)%4
+    elif dirc == 'F': continue
+    answer[0] += dr[idx]
+    answer[1] += dc[idx]
+
+print(*answer,sep=' ')

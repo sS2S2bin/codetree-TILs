0af1017137b2a553b54cd445ac4df50fa1566203
Soobin DD, (Designer + Developer) <52414079+sS2S2bin@users.noreply.github.com>
2024-04-13 23:41:29 +0900

    Title: 불안한 무빙워크, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240413/불안한 무빙워크/unstable-moving-walk.py
@@ -0,0 +1,58 @@
+"""
+1번에서 n번에 내림, 안정성 -1씩 감소, 안정성 0이면 못올라감
+ㄱ. 무빙워크 회전
+ㄴ. 안정성 0 아니면 이동
+ㄷ. 첫 번째 칸에 사람이 없고 안정성 0이 아니면 한명 더(한 번에 한 사람??)
+ㄹ. 0이 k개 이상이면 종료
+
+1. 아이디어
+2. 시간복잡도
+3. 자료구조
+"""
+from collections import deque
+n,k = list(map(int, input().split()))
+arr = deque(map(int, input().split()))
+whereman = []
+cnt = 1
+zerocnt = 0
+
+arr.rotate(1)
+whereman.append(0)
+arr[0]-=1
+if arr[0]==0:
+    zerocnt +=1
+
+while(zerocnt<k):
+    cnt +=1
+    # 회전
+    arr.rotate(1)
+    # whereman = (whereman + 1)%(2*n)
+    # print('arr:',arr)
+
+    # 앞에 사람이 없고, 0이 아니면 이동
+    for i,idx in enumerate(whereman):
+        idx = (idx+1)%(n) #rotate 한거 업데이트 지금하깅
+        # print(i,idx, whereman)
+        if idx == n-1:
+            whereman.pop(i)
+        else:
+            if (idx+1) not in whereman and arr[idx+1]>0:
+                arr[idx+1] -=1
+                if arr[idx+1]==0:
+                    zerocnt +=1
+                if (idx+1)==(n-1): 
+                    del whereman[i]
+                elif(idx+1)!=(n-1):
+                    whereman[i]=idx+1
+                    # print('이동 후',whereman)
+
+    # 1번에 사람이 없고 안정성 ok 면 사람 추가
+    if 0 not in whereman and arr[0]!=0:
+        whereman.append(0)
+        arr[0] -=1
+        if arr[0]==0:
+            zerocnt+=1
+    # print('where:',whereman)
+# print("answer : {}, zero cnt : {}, k:{} , arr:{}".format(cnt,zerocnt,k,arr))
+
+print(cnt)
